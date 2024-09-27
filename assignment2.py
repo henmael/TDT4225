b1585e9fcc301d43812f610a626fd9abbc900b1b
Henriette Eltvik <henmael@outlook.com>
2024-09-27 11:33:17 +0200

    Insert into User table

--- assignment2.py
+++ assignment2.py
@@ -1,5 +1,6 @@
 from DbConnector import DbConnector
 from tabulate import tabulate
+import os
 
 class Trajector:
     
@@ -44,13 +45,46 @@ class Trajector:
         rows = self.cursor.fetchall()
         print(tabulate(rows, headers=self.cursor.column_names))
 
+    def insert_data_user(self):
+        labeled_txt_user = []
+        user_ids_labels = []
+        
+        with open('C:/Users/henri/Desktop/dataset/labeled_ids.txt') as labeled_file_user:
+            labeled_txt_user = labeled_file_user.read().splitlines()[::-1]
+        
+        for (root,dirs,files) in os.walk('C:/Users/henri/Desktop/dataset', topdown=True):
+            if 'labels.txt' in files:
+                print(f"Found labels.txt in: {root}")
+                parts = root.replace('\\', '/').split('/')
+                endpart = parts[-1]
+                if endpart in labeled_txt_user:
+                    print(f"Right user_id {endpart}")
+                    user_ids_labels.append((endpart,True))
+                else: 
+                    print(f"Wrong user_id {endpart}")
+            else:
+                parts2 = root.replace('\\', '/').split('/')
+                if (len(parts2) == 7):
+                    endpart = parts2[-1]
+                    print(f"User Id without label {endpart}")
+                    user_ids_labels.append((endpart, False))
+        
+        
+        query = "INSERT INTO User (id, has_label) VALUES ('%s', %s)"
+
+        for id, has_label in user_ids_labels:
+            self.cursor.execute(query % (id, has_label))
+        
+        self.db_connection.commit()
+
 
 def main():
     program = None
     try:
         program = Trajector()
-        program.create_table()
-        program.show_tables()
+        #program.create_table()
+        #program.show_tables()
+        program.insert_data()
     except Exception as e:
         print("ERROR: Failed to use database:", e)
     finally:
