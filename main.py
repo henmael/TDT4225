bc1d15c865b4246252f75a8da16a4f84f1721d38
Alexander Moltu <alexandermoltu@gmail.com>
2024-10-04 11:18:49 +0200

    finished task 5

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from part2.task1 import task1_main
 from part2.task2 import task2_main
 from part2.task3 import task3_main
 from part2.task4 import task4_main
+from part2.task5 import task5_main
 
 
 if __name__ == '__main__':
@@ -10,15 +11,17 @@ if __name__ == '__main__':
     # trajector_main()
 
     # Task 1
-    task1_main()
+    # task1_main()
 
     # Task 2
-    task2_main()
+    # task2_main()
 
     # Task 3
-    task3_main()
+    # task3_main()
 
     # Task 4
-    task4_main()
+    # task4_main()
 
+    # Task 5
+    task5_main()
 
---
+++ part2/task5.py
@@ -0,0 +1,36 @@
+from DbConnector import DbConnector
+from tabulate import tabulate
+
+# Find all types of transportation modes and count how many activities that are
+# tagged with these transportation mode labels. Do not count the rows where
+# the mode is null.
+
+
+class Task5:
+    def __init__(self):
+        self.connection = DbConnector()
+        self.db_connection = self.connection.db_connection
+        self.cursor = self.connection.cursor
+    
+    def retrieve_users_taxi(self):
+        array = []
+        query = """
+            SELECT transportation_mode, COUNT(*) AS mode_count
+            FROM Activity
+            WHERE transportation_mode IS NOT NULL
+            GROUP BY transportation_mode
+            ORDER BY mode_count DESC;
+        """
+
+        self.cursor.execute(query)
+        user_activity_taxi = self.cursor.fetchall()
+        
+        for k, v in user_activity_taxi:
+            array.append([k, v])
+
+        print(tabulate(array, headers=["User ID", "Transportation"]))
+
+def task5_main():
+    program = Task5()
+    program.retrieve_users_taxi()
+    program.connection.close_connection()
