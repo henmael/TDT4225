6b97a2f36fee78ff516513bad1fb2977de825a2a
Alexander Moltu <alexandermoltu@gmail.com>
2024-10-04 14:37:50 +0200

    updated main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from assignment2 import trajector_main
 from part2.task1 import task1_main
-from part2.task11 import task11_main
 from part2.task2 import task2_main
 from part2.task3 import task3_main
 from part2.task4 import task4_main
@@ -8,7 +7,9 @@ from part2.task5 import task5_main
 from part2.task6 import task6_main
 from part2.task7 import task7_main
 from part2.task8 import task8_main
+from part2.task9 import task9_main
 from part2.task10 import task10_main
+from part2.task11 import task11_main
 
 if __name__ == '__main__':
     # Insert tables and values in database
