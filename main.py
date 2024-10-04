bb821c71a3afd5ee4864660166842029ccfb2ffc
Henriette Eltvik <henmael@outlook.com>
2024-10-04 14:42:23 +0200

    la til tabell format task7

--- main.py
+++ main.py
@@ -9,40 +9,41 @@ from part2.task6 import task6_main
 from part2.task7 import task7_main
 from part2.task8 import task8_main
 from part2.task10 import task10_main
+from part2.task9 import task9_main
 
 if __name__ == '__main__':
     # Insert tables and values in database
     # trajector_main()
 
-    # Task 1
-    task1_main()
+    # # Task 1
+    # task1_main()
 
-    # Task 2
-    task2_main()
+    # # Task 2
+    # task2_main()
 
-    # Task 3
-    task3_main()
+    # # Task 3
+    # task3_main()
 
-    # Task 4
-    task4_main()
+    # # Task 4
+    # task4_main()
 
-    # Task 5
-    task5_main()
+    # # Task 5
+    # task5_main()
     
-    # Task 6
-    task6_main()
+    # # Task 6
+    # task6_main()
 
     # Task 7
     task7_main()
 
     # Task 8
-    task8_main()
+    # task8_main()
 
-    # Task 9
-    task9_main()
+    # # Task 9
+    # task9_main()
 
-    # Task 10
-    task10_main()
+    # # Task 10
+    # task10_main()
 
-    # Task 11
-    task11_main()
+    # # Task 11
+    # task11_main()
