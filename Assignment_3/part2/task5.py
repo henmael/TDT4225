e3428378f1aff305ba9e4027ef482fc6667ab7ad
Henriette Eltvik <henmael@outlook.com>
2024-10-30 22:07:43 +0100

    ferdig task 5

--- Assignment_3/part2/task5.py
+++ Assignment_3/part2/task5.py
@@ -6,9 +6,31 @@ class Task5:
         self.client = self.connection.client
         self.db = self.connection.db
 
+    def amount_activities_per_transportation(self, collection_name):
+        collection = self.db[collection_name]
+        pipeline = [
+            {
+                '$match': {
+                    'transportation_mode': {'$ne': None}
+                }
+            },{
+                '$group': {
+                    '_id': '$transportation_mode',
+                    'count': {
+                        '$sum': 1
+                    }
+                }
+            }
+        ]
+
+        activity_per_user = collection.aggregate(pipeline)
+
+        for transportation_activities in activity_per_user:
+            print(transportation_activities)
+
 
 def task5_main():
     program = Task5()
-    # add relevant function(s) here
+    program.amount_activities_per_transportation('Activity')
     program.connection.close_connection()
 
